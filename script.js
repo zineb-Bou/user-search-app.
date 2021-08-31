e87a1fda1577ff6cece6880452ea50a104b15c28
zineb-Bou <36958804+zineb-Bou@users.noreply.github.com>
2021-08-31 19:27:14 +0100

    Add some js to implement body transition.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const theme = document.querySelector('#theme-link');
 
 const input = document.querySelector('#input_switch');
-
+const icon = document.querySelector('.icon-name');
 input.addEventListener('change', function () {
   if (theme.getAttribute('href') == 'css/Light_theme.css') {
     theme.href = 'css/Dark_theme.css';
+    icon.textContent = 'Light';
+    document.body.classList.add('active');
   } else {
     theme.href = 'css/Light_theme.css';
+    icon.textContent = 'Dark';
+    document.body.classList.add('active');
   }
 });
