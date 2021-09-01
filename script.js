30069146626589053da06e26359387775b0ccaf0
zineb-Bou <36958804+zineb-Bou@users.noreply.github.com>
2021-09-01 10:37:42 +0100

    Add comments to make the code more readable :page_facing_up:

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+// Variables
 const theme = document.querySelector('#theme-link');
-
 const input = document.querySelector('#input_switch');
 const icon = document.querySelector('.icon-name');
+
+// When clicking on the toggle switch btn
 input.addEventListener('change', function () {
+  // if the current theme is light theme.
   if (theme.getAttribute('href') == 'css/Light_theme.css') {
     theme.href = 'css/Dark_theme.css';
+    // change the inner text to -light-
     icon.textContent = 'Light';
-    document.body.classList.add('active');
+    // Add bg_transition class to the body to implement transition when switching between themes
+    document.body.classList.add('bg_transition');
   } else {
     theme.href = 'css/Light_theme.css';
+    // change the inner text to -Dark-
     icon.textContent = 'Dark';
-    document.body.classList.add('active');
+    // Add bg_transition class to the body to implement transition when switching between themes
+    document.body.classList.add('bg_transition');
   }
 });
